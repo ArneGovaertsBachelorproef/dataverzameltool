6ab985e45cee22d7564e09c4abf68c94720e6bce
Arne Govaerts <arne.govaerts@student.hogent.be>
2022-02-05 18:59:20 +0100

    view

---
+++ recorder.js
@@ -0,0 +1,25 @@
+const recordAudio = () =>
+    new Promise(async resolve => {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        const mediaRecorder = new MediaRecorder(stream);
+        const audioChunks = [];
+
+        mediaRecorder.addEventListener("dataavailable", event => {
+            audioChunks.push(event.data);
+        });
+
+        const start = () => mediaRecorder.start();
+
+        const stop = () =>
+            new Promise(resolve => {
+                mediaRecorder.addEventListener("stop", () => {
+                    const audioBlob = new Blob(audioChunks, { type: 'audio/ogg; codecs=opus' });
+                    const audioUrl = URL.createObjectURL(audioBlob);
+                    resolve({ audioBlob, audioUrl });
+                });
+
+                mediaRecorder.stop();
+            });
+
+        resolve({ start, stop });
+    });
